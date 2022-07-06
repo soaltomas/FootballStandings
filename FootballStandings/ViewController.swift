6940ec402bc1a2faceb4b0cf7291b702e52acede
Artem Polushin <soaltomas@gmail.com>
2022-07-06 13:52:08 +0400

    Initial Commit

---
+++ FootballStandings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootballStandings
+//
+//  Created by Артем Полушин on 06.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
